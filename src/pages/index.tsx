212fbf467460f92608e6097c7c7f5e5f9868d8b9
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-02-13 09:55:32 -0300

    ajustes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,16 +46,16 @@ export default function Home() {
             </div>
             <figure className="relative flex justify-center xl:-mr-30">
               <div className="circle-decoration-active absolute w-[400px] h-[400px] z-[1] left-1/2 top-1/2 -translate-x-1/2 -translate-y-[50%] xl:w-[600px] xl:h-[600px]"></div>
-              <Image
+              <img
                 src={"/img/hero-photo.webp"}
                 alt={"Douglas Soave"}
                 className="w-full h-full object-cover z-[2] relative max-w-[300px] lg:max-w-[400px] xl:max-w-[540px]"
-                loader={imageLoader}
+                // loader={imageLoader}
                 width={540}
                 height={729}
-                decoding={"sync"}
-                priority={true}
-                unoptimized
+                // priority={true}
+                // unoptimized
+                decoding="sync"
               />
             </figure>
           </div>
