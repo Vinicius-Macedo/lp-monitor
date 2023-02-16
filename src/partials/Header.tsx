56908f5d5078d20d7406b6a73dcef864c26be3cf
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-02-16 09:36:44 -0300

    commit

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -5,15 +5,16 @@ const imageLoader = require("./../loader.js");
 
 export function Header() {
   return (
-    <header className="flex justify-between w-full max-w-[1110px] px-16 xl:px-0 pt-16 m-auto">
-      <Image
-        src={"/img/monitor-logo-white.webp"}
-        alt={"Logo do monitor do mercado"}
-        width={190}
-        height={60}
-        loader={imageLoader}
-        className="object-contain"
-      />
-    </header>
+    <div></div>
+    // <header className="flex justify-between w-full max-w-[1110px] px-16 xl:px-0 pt-16 m-auto">
+    //   <Image
+    //     src={"/img/monitor-logo-white.webp"}
+    //     alt={"Logo do monitor do mercado"}
+    //     width={190}
+    //     height={60}
+    //     loader={imageLoader}
+    //     className="object-contain"
+    //   />
+    // </header>
   );
 }
