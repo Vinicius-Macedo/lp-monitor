1bcdcd704050fa16e8adfd0a182fad7cccdbd7f7
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-02-17 13:16:56 -0300

    ajustes

--- src/components/SimpleCard.tsx
+++ src/components/SimpleCard.tsx
@@ -5,5 +5,5 @@ type SimpleCardProps = {
 };
 
 export function SimpleCard(props: SimpleCardProps) {
-  return <div className="bg-[#525252] text-white p-8 text-[.875rem]">{props.description}</div>;
+  return <div className="bg-[#525252] text-white p-8 text-[.875rem] italic">"{props.description}"</div>;
 }
