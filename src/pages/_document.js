28bf4c86c9642f2e4ed8a1b3b60b3f3f58f3622e
vinicius-macedo <vinicius.silva1076@etec.sp.gov.br>
2023-02-17 13:23:16 -0300

    ajustes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap"
           rel="stylesheet"
         />
       </Head>
